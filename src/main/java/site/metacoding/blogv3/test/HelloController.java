23e98f3b4921074e6f76155631211987656097a1
yuseunghyun <ysh7091@gmail.com>
2022-04-11 10:10:26 +0900

    header, footer 완성

--- src/main/java/site/metacoding/blogv3/test/HelloController.java
+++ src/main/java/site/metacoding/blogv3/test/HelloController.java
@@ -1,13 +1,14 @@
 package site.metacoding.blogv3.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String test() {
-        return "test";
+        return "main";
     }
 }
